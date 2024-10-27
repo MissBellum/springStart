a2be78349652a6b742a300d10774cd7ac25b6b55
Missbellum <enjaykanz@gmail.com>
2024-10-27 23:06:42 +0100

    Ran successful IntelliJ build

--- cashcard/src/main/java/example/cashcard/CashcardApplication.java
+++ cashcard/src/main/java/example/cashcard/CashcardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashcardApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CashcardApplication.class, args);
 	}
 
