c01bf71133f85ee3fa29d877cd1d064d3893eda7
Missbellum <enjaykanz@gmail.com>
2024-10-29 23:17:06 +0100

    Changed JsonTest json file path to relative path

--- cashcard/src/test/java/example/cashcard/CashcardJsonTest.java
+++ cashcard/src/test/java/example/cashcard/CashcardJsonTest.java
@@ -1,9 +1,11 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
-import static  org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
 import org.springframework.boot.test.json.JacksonTester;
 import java.io.IOException;
 
@@ -16,7 +18,7 @@ class CashcardJsonTest {
     void cashCardSerializationTest() throws IOException {
         CashcardApplication cashCard = new CashcardApplication(99L, 123.45);
 
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("src/test/resources/expected.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
